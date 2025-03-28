bbda0621b4dfff93953c33c4e01b69bb54c59f02
Isaque <isaqueladeiraesteves@gmail.com>
2025-03-27 23:01:15 -0300

    refiz a maioria do codigo e peguei uma orientação do DevClub

---
+++ script.js
@@ -0,0 +1,36 @@
+var button = document.querySelector(".add-task");
+var input = document.querySelector(".input-task");
+var lista = []
+var listaok = document.querySelector(".list-task")
+function criarli(){
+    lista.push({
+        tarefa: input.value,
+        concluida: false
+    })
+    input.value = ""
+
+    mostrarli()
+
+}
+function mostrarli(){
+    var newTask = ""
+    lista.forEach((task, posicao) => {
+    newTask += `<li class="task ${task.concluida && "done"}">
+            <img src="okimagem.png" alt="imagem ok" onclick="feito(${posicao})">
+            <p>${task.tarefa}</p>
+            <img src="lixo.png" alt="imagem lixo" onclick="deleteItem(${posicao})">
+        </li>`
+    }) 
+    listaok.innerHTML = newTask
+    input.value = ""
+}
+button.addEventListener("click",  criarli);
+
+function deleteItem(posicao){
+    lista.splice(posicao,1)
+    mostrarli()
+}
+function feito(posicao){
+    lista[posicao].concluida = !lista[posicao].concluida
+    mostrarli()
+}
